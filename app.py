5292feb87e3cabb24df2c79ea02d04a1f497488d
chaikalef <chaikalef@gmail.com>
2020-04-27 17:36:06 +0300

    add flasgger

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+from flasgger import Swagger, swag_from
 from flask import Flask, jsonify, request
 
 from ml import Clf
 from validate import e_400, e_500, validate
 
 app = Flask(__name__)
+swagger = Swagger(app)
 clf = Clf()
 
 app.register_error_handler(400, e_400)
@@ -11,8 +13,9 @@ app.register_error_handler(500, e_500)
 
 
 @app.route('/predict', methods=['POST'])
+@swag_from('swag.yaml')
 @validate
-def pred():
+def predict():
     req = request.get_json()
     res = []
 
